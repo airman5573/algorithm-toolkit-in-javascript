73d81e5977b46edea566b3306a80b20c21361d46
airman5573 <rpf5573@kakao.com>
2021-12-22 03:29:43 +0900

    세그먼트트리 - 구간합 구하기 문제 추가

---
+++ 테크닉/세그먼트-트리/구간-합-구하기/README.md
@@ -0,0 +1,8 @@
+# 구간 합 구하기
+## 문제
+[백준 - 구간 합 구하기(2042)](https://www.acmicpc.net/problem/2042)
+
+## 풀이
+
+## 코드
+[answer.js](./answer.js)
---
+++ 테크닉/세그먼트-트리/구간-합-구하기/answer.js
@@ -0,0 +1,19 @@
+// segment-tree-range-sum.js 파일의 SegmentTree class를 가져다 사용했다
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [CHANGE, GETSUM] = [1, 2];
+const [N, M, K] = input[0].split(' ').map(Number);
+const arr = new Array(N);
+for (let i = 0; i < N; i += 1) {
+  arr[i] = parseInt(input[i+1], 10);
+}
+const segmentTree = new SegmentTreeSum(arr);
+for (let i = N + 1; i < N + M + K + 1; i += 1) {
+  const [a, b, c] = input[i].split(' ').map(Number);
+  if (a === CHANGE) {
+    segmentTree.update(b - 1, c);
+  } else {
+    console.log(segmentTree.getSum([b-1, c-1])); 
+  }
+}
