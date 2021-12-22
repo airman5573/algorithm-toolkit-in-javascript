6f696741891c410c6f92ffc746404ec0d3d9b136
airman5573 <rpf5573@kakao.com>
2021-12-22 11:56:31 +0900

    refactor(세그먼트-트리): 세그먼트 트리 클래스를 genral하게 만든다

--- 테크닉/세그먼트-트리/segment-tree-range-sum.js
+++ 테크닉/세그먼트-트리/segment-tree-range-sum.js
@@ -1,24 +1,8 @@
-class SegmentTreeSum {
-  constructor(numArr) {
-    this.numArr = numArr;
-    this.tree = new Array(numArr.length * 4); // 최대 길이가 배열길이 x 4보다는 작다.
-    this.build();
-  }
-  
-  build() {
-    const _build = (node, range) => {
-      const [left, right] = range;
-      // 끝점에 다달았을때 값을 집어넣는다
-      if (left === right) {
-        return this.tree[node] = this.numArr[left];
-      }
-      const mid = Math.floor((left + right)/2);
-      const [leftChildNode, rightChildNode] = [node * 2 + 1, node * 2 + 2];
-      const leftVal = _build(leftChildNode, [left, mid]);
-      const rightVal = _build(rightChildNode, [mid + 1, right]);
-      return this.tree[node] = leftVal + rightVal;
-    };
-    _build(0, [0, this.numArr.length - 1]);
+import SegmentTree from './segment-tree.js';
+
+class SegmentTreeSum extends SegmentTree {
+  merge(leftVal, rightVal) {
+    return leftVal + rightVal;
   }
 
   getSum(section) {
@@ -65,3 +49,5 @@ class SegmentTreeSum {
     _update(0, [0, this.numArr.length - 1]);
   }
 }
+
+export default SegmentTreeSum;
