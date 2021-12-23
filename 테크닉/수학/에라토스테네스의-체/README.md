7bc396acf4bcd708607ca593f1c59eaa007c5f1b
airman5573 <rpf5573@kakao.com>
2021-12-24 04:31:13 +0900

    feat(수학): 소수찾는 방법(에라스토테네스의 체) 추가

---
+++ 테크닉/수학/에라토스테네스의-체/README.md
@@ -0,0 +1,8 @@
+# 에라토스테네스의 체
+## 설명
+- [[중1상][교과서][개념][01] 소인수분해 [02] 에라토스테네스의 체](https://www.youtube.com/watch?v=sV3-jnfaW2E)
+- [에라토스테네스의 체를 통해 소수 문제 정복 :: 마이구미](https://mygumi.tistory.com/66)
+- [[에어맨]에라토스테네스의 체에서 제곱근 까지만 루프를 도는 이유](https://develoger.kr/algorithm/%ec%97%90%eb%9d%bc%ed%86%a0%ec%8a%a4%ed%85%8c%eb%84%a4%ec%8a%a4%ec%9d%98-%ec%b2%b4%ec%97%90%ec%84%9c-%ec%a0%9c%ea%b3%b1%ea%b7%bc-%ea%b9%8c%ec%a7%80%eb%a7%8c-%eb%a3%a8%ed%94%84%eb%a5%bc-%eb%8f%84/)
+
+## 구현
+[prime-tester.js](./prime-tester.js)
---
+++ 테크닉/수학/에라토스테네스의-체/prime-tester.js
@@ -0,0 +1,22 @@
+function getPrimeTester(n) {
+  const [IM_PRIME, IM_NOT_PRIME] = [1, 0];
+  // 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+  n = n + 1 // 이렇게 하면 소수목록에서 조회할때 index - 1 안해줘도 된다
+  const tester = new Array(n);
+  for (let i = 0; i < n; i += 1) {
+    tester[i] = IM_PRIME;
+  }
+  // 1은 소수가 아니기 때문에, 2부터 조사한다
+  // n의 제곱근까지만 루프를 돌아도 되는 이유는 REAME.md를 참고한다
+  for (let i = 2; i < parseInt(n ** 0.5) + 1; i += 1) {
+    if (tester[i]) {
+      // 위에서 n = n + 1을 해주었기 때문에, n은 포함 안시켜도 된다.
+      for (let j = 2; j < n; j += 1) {
+        tester[i * j] = IM_NOT_PRIME;
+      }
+    }
+  }
+  return tester;
+}
+
+export default getPrimeTester;
