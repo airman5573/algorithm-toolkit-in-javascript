fdbac69affd5fec076281e328083c70990c5e332
airman5573 <rpf5573@kakao.com>
2021-12-28 11:44:53 +0900

    fix(기타등등-range): range함수에 step을 추가한다

--- 분류/기타등등/range.js
+++ 분류/기타등등/range.js
@@ -1,7 +1,10 @@
-function range(_from, to) {
-  let N = to + 1, i = _from, arr = Array(N);
-  while (i < N) {
-    arr[i++] = i;
+function range(_from, to, step = 1) {
+  let n = Math.floor((to - _from) / step) + 1
+  let i = 0;
+  const arr = Array(n);
+  while (i < n) {
+    arr[i] = _from + i * step;
+    i += 1;
   }
   return arr;
 }
