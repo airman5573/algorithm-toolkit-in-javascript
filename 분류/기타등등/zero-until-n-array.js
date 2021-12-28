616e73edd2a53f8a5abbc413775d5b104270a47b
airman5573 <rpf5573@kakao.com>
2021-12-28 11:33:01 +0900

    feat(기타등등): 배열 관련 메소드 삭제

--- 분류/기타등등/zero-until-n-array.js
+++
@@ -1,8 +0,0 @@
-// 출처 : https://stackoverflow.com/a/33352604/9279003
-
-function zeroUntilNArray(n) {
-  //=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  return Array.from(Array(10).keys());
-}
-
-export default zeroUntilNArray;
